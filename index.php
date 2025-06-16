167369d78c641e00b8f2e283e523ea49694becea
D. Sidhant Patro <102447769+Sidopolis@users.noreply.github.com>
2025-06-16 06:28:25 +0000

    i did some changes

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
-echo "Hello from CodePipeline!";
+echo "<!DOCTYPE html>";
+echo "<html><head><title>Deployed!</title></head><body style='text-align:center;padding-top:100px;font-family:sans-serif'>";
+echo "<h2 style='color:green;'>✅ Deployed Successfully via CodePipeline</h2>";
+echo "</body></html>";
 ?>
